34077c853c3278386f689e233e7a2737c926bb0f
SzymonDabrowski <szymon.h.dabrowski@gmail.com>
2024-01-18 16:29:53 +0100

    Use csv_reader

--- csv_wrapper.py
+++ csv_wrapper.py
@@ -1,44 +1,9 @@
 import argparse
-import csv
-import chardet
+import csv_reader
 from datetime import datetime
 from enum import Enum
 import logging
 from typing import Set, Dict, List, Union
-
-class Csv:
-    @staticmethod
-    def read(filename: str) -> List[List[str]]:
-        encoding = 'utf-8'
-        with open(filename, 'rb') as raw_file:
-            encoding = chardet.detect(raw_file.read(1024))['encoding']
-
-        with open(filename, 'r', newline='', encoding=encoding) as file:
-            dialect = csv.Sniffer().sniff(file.read(4096)) # throws
-            file.seek(0)
-            reader = csv.reader(file, delimiter=dialect.delimiter, quotechar=dialect.quotechar)
-            data = list(reader)
-        return data
-    
-    @staticmethod
-    def export(data: List[List[str]], filename: str) -> None:
-        with open(filename, 'w', newline='', encoding='utf-8') as file:
-            writer = csv.writer(file, delimiter='\t', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-            for row in data:
-                writer.writerow(row)
-
-    @staticmethod
-    def get_column_names(filename: str):
-        encoding = 'utf-8'
-        with open(filename, 'rb') as raw_file:
-            encoding = chardet.detect(raw_file.read(1024))['encoding']
-
-        with open(filename, 'r', newline='', encoding=encoding) as file:
-            dialect = csv.Sniffer().sniff(file.read(4096)) # throws
-            file.seek(0)
-            reader = csv.reader(file, delimiter=dialect.delimiter, quotechar=dialect.quotechar)
-            data = list(reader)
-        return data[0]
 
 class Bank:
     class Name(Enum):
@@ -257,8 +222,8 @@ def main():
     args = parser.parse_args()
 
     filename = args.filename
-    csv_data = Csv.read(filename)
-    bank = Bank(Csv.get_column_names(filename))
+    csv_data = csv_reader.Csv.read(filename)
+    bank = Bank(csv_reader.Csv.get_column_names(filename))
     data_without_column_names = remove_column_names(csv_data)
     filtered_and_sorted_data = filter_and_sort_data(data_without_column_names, bank)
 
@@ -279,8 +244,8 @@ def main():
         ambiguous_data = []
         filter_ambiguous_data(output_data, ambiguous_data)
 
-        Csv.export(output_data, args.output)
-        Csv.export(ambiguous_data, args.output + "_ambiguous.csv")
+        csv_reader.Csv.export(output_data, args.output)
+        csv_reader.Csv.export(ambiguous_data, args.output + "_ambiguous.csv")
     elif bank.name == Bank.Name.MILLENNIUM:
         print("Not implemented yet")
     else:
